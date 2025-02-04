a6c21a2acebc0428beb13fa67b7ea4c36f2cae46
Gabi <gabi@Xaviers-MacBook-Pro.local>
2025-02-04 12:50:02 +0100

    unify WebDriver initialization and cleanup in IntermediateLevelTest

--- src/test/java/seleniumproject/IntermediateLevelTest.java
+++ src/test/java/seleniumproject/IntermediateLevelTest.java
@@ -1,67 +1,61 @@
 package seleniumproject;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class IntermediateLevelTest {
 
-private ChromeDriver driver;
-private FirefoxDriver driverFirefox;
-JavascriptExecutor js;
-private boolean useChrome = true; // Cambia a 'true' si quieres usar Chrome
+    private WebDriver driver;
+    private JavascriptExecutor js;
+    private boolean useChrome = false; // Cambia a 'true' si quieres usar Chrome
 
-@Before
-public void setUp() {
-    if (useChrome) {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    @Before
+    public void setUp() {
+        if (useChrome) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
         js = (JavascriptExecutor) driver;
-    } else {
-        WebDriverManager.firefoxdriver().setup();
-        driverFirefox = new FirefoxDriver();
-        js = (JavascriptExecutor) driverFirefox;
     }
-}
 
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
-@After
-public void tearDown() {
-    if(useChrome) {
-        driver.quit();
-}else if(!useChrome){
-    driverFirefox.quit();
-}
-}
+    @Test
+    public void studentRegistrationForm() {
+        driver.get("https://demoqa.com/automation-practice-form");
+        driver.findElement(By.id("firstName")).click();
+        driver.findElement(By.id("firstName")).sendKeys("Gabriela");
+        driver.findElement(By.id("lastName")).click();
+        driver.findElement(By.id("lastName")).sendKeys("Pineiro");
+        driver.findElement(By.cssSelector(".custom-radio:nth-child(2) > .custom-control-label")).click();
+        driver.findElement(By.id("userNumber")).click();
+        driver.findElement(By.id("userNumber")).sendKeys("1234567890");
+        js.executeScript("arguments[0].click();", driver.findElement(By.id("submit")));
+        js.executeScript("arguments[0].click();", driver.findElement(By.id("closeLargeModal")));
+    }
 
-@Test
-public void studentRegistrationForm(){
-    driver.get("https://demoqa.com/automation-practice-form");
-    driver.findElement(By.id("firstName")).click();
-    driver.findElement(By.id("firstName")).sendKeys("Gabriela");
-    driver.findElement(By.id("lastName")).click();
-    driver.findElement(By.id("lastName")).sendKeys("Pineiro");
-    driver.findElement(By.cssSelector(".custom-radio:nth-child(2) > .custom-control-label")).click();
-    driver.findElement(By.id("userNumber")).click();
-    driver.findElement(By.id("userNumber")).sendKeys("1234567890");
-    js.executeScript("arguments[0].click();", driver.findElement(By.id("submit")));
-    js.executeScript("arguments[0].click();", driver.findElement(By.id("closeLargeModal")));
-}
-
- @Test
-  public void alerts() {
-    driverFirefox.get("https://demoqa.com/alerts");
-    driverFirefox.manage().window().setSize(new Dimension(1472, 842));
-    driverFirefox.findElement(By.id("alertButton")).click();
-    assertEquals("You clicked a button", driverFirefox.switchTo().alert().getText());
-  }
+    @Test
+    public void alerts() {
+        driver.get("https://demoqa.com/alerts");
+        driver.manage().window().setSize(new Dimension(1472, 842));
+        driver.findElement(By.id("alertButton")).click();
+        assertEquals("You clicked a button", driver.switchTo().alert().getText());
+    }
 }
