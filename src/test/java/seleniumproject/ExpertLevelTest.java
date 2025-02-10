ce468df4dc25cb26180df1b5d78f3b3959f6a333
Gabi <gabi@Xaviers-MacBook-Pro.local>
2025-02-10 12:24:36 +0100

    AUT-24 update ExpertLevelTest to support multi-browser testing and remove unused configurations

--- src/test/java/seleniumproject/ExpertLevelTest.java
+++ src/test/java/seleniumproject/ExpertLevelTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.openqa.selenium.By;
-// import org.junit.jupiter.api.ParameterizedTest;
-// import org.junit.jupiter.params.provider.ValueSource;
-import org.openqa.selenium.JavascriptExecutor;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -16,43 +18,38 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class ExpertLevelTest {
     private WebDriver driver;
     JavascriptExecutor js;
-    private boolean useChrome = false; // Cambia a 'true' si quieres usar Chrome
+    boolean useChrome = true; // Cambia a 'true' si quieres usar Chrome
+    
 
     @BeforeEach
     public void setUp() {
-        if (useChrome) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        } else {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        }
-        js = (JavascriptExecutor) driver;
+      
     }
-    // @ParameterizedTest
-    // @ValueSource(strings = {"chrome", "firefox"})
-    // public void testInDifferentBrowsers(String browser) {
-    //     if (browser.equals("chrome")) {
-    //         WebDriverManager.chromedriver().setup();
-    //         driver = new ChromeDriver();
-    //     } else if (browser.equals("firefox")) {
-    //         WebDriverManager.firefoxdriver().setup();
-    //         driver = new FirefoxDriver();
-    //     }
-
-    //     tearDown();
-    // }
 
     @AfterEach
     public void tearDown() {
         driver.quit();
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"chrome", "firefox"})
+    void multiBrowser(String browser) {
+        WebDriverManager.getInstance(browser.equals("chrome") ? ChromeDriver.class : FirefoxDriver.class).setup();
+        driver = browser.equals("chrome") ? new ChromeDriver() : new FirefoxDriver();
+        
+        driver.get("https://www.selenium.dev/");
+        driver.findElement(By.id("Layer_1")).isDisplayed();
+    }
+
     @Test
     public void deleteUserFromDinamicTable() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(); // Initialize the driver
         driver.get("https://demoqa.com/webtables");
         driver.findElement(By.id("delete-record-1")).click();
         boolean isUserDeleted = driver.findElements(By.xpath("//div[text()='Cierra']")).isEmpty();
         assertEquals(true, isUserDeleted);
     }
+  
+
 }
